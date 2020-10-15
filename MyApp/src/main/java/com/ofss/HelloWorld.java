db2bcbcb3e7798565dea039d5b13f1a7bc2ef5a9
Pradyumna <pradyumnapandey174@gmail.com>
2020-10-15 14:43:01 +0530

    second commit

--- MyApp/src/main/java/com/ofss/HelloWorld.java
+++ MyApp/src/main/java/com/ofss/HelloWorld.java
@@ -2,7 +2,7 @@ package com.ofss;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("this is my first commint on git using eclippse");
+		System.out.println("this is my first commint on git using eclipse");
 	}
 
 }
