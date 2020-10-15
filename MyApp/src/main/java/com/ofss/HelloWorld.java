4df748dedc6efcb69a7e62f5a8b35adc890985f8
Pradyumna <pradyumnapandey174@gmail.com>
2020-10-15 14:58:50 +0530

    third commit

--- MyApp/src/main/java/com/ofss/HelloWorld.java
+++ MyApp/src/main/java/com/ofss/HelloWorld.java
@@ -3,6 +3,8 @@ package com.ofss;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("this is my first commint on git using eclipse");
+		Customer customer=new Customer("PP","Mumbai");
+		System.out.println(customer);
 	}
 
 }
